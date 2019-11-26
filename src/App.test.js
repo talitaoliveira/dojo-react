863573579f2008625babff365834d0d128b9dadf
Talita Oliveira <litaa.oliveira@gmail.com>
2019-11-26 10:08:53 -0300

    Include simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+describe('header', () => {
+  it('renders one element header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('header')).toHaveLength(1);
+  });
 });
