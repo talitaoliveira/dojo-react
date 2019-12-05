1b6b2610f58cad4d5c390d9971599898a1810bc1
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2019-12-05 10:23:22 -0300

    add header component with first unit test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header title="POKE"/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
