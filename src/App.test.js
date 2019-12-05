ce507c324120b3cd9b653af4a00a3143bed34bd3
ShellOliver <sheldon.santosdev@gmail.com>
2019-12-05 09:24:21 -0300

    add eslint standard

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import App from './App'
+
+describe('header', () => {
+  it('renders one element header', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('header')).toHaveLength(1)
+  })
+})
