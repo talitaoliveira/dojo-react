dcecd20668981a1934bc040b3a62bc784cdf5f62
ShellOliver <sheldon.santosdev@gmail.com>
2019-12-05 09:19:42 -0300

    add setupTests to remove settings from spec files

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,11 +1,7 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
-
 import React from 'react';
 import App from './App';
 
-configure({ adapter: new Adapter() });
 describe('header', () => {
   it('renders one element header', () => {
     const wrapper = shallow(<App />);
