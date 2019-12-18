7e58f5a302d2a0d77b55b7c33554a03f7be48f7e
Talita Oliveira <litaa.oliveira@gmail.com>
2019-12-18 12:54:46 -0300

    Starting to create the pokemon list

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,41 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import App from './App'
+import List from './List'
 import Header from './Header'
+
+const getWrapper = (props) => shallow(
+  <App pokemon={[]}
+    {...props}
+  />
+)
 
 describe('header', () => {
   it('renders a header with a title', () => {
-    const wrapper = shallow(<App />)
+    const wrapper = getWrapper()
     expect(wrapper.find(Header).exists()).toBeTruthy()
     expect(wrapper.find(Header).props().title).toEqual('POKE')
   })
+})
 
+describe('list', () => {
+  it('renders the pokemon list', () => {
+    const wrapper = getWrapper({ pokemon: ['Pikachu', 'Mew'] })
+    expect(wrapper.find('.pokemon-name').at(0).text()).toEqual('Pikachu')
+    expect(wrapper.find('.pokemon-name').at(1).text()).toEqual('Mew')
+  })
+  it('renders the pokemon list component', () => {
+    const wrapper = getWrapper({ pokemon: ['Pikachu', 'Mew'] })
+    expect(wrapper.find(List).exists()).toBeTruthy()
+  })
+})
+
+describe('details', () => {
+  describe('when I click on the Pokemon', () => {
+    it('renders the pokemon details ', () => {
+      const wrapper = getWrapper({ pokemon: ['Pikachu', 'Mew'] })
+      wrapper.find('.pokemon-name').at(0).simulate('click')
+      expect(wrapper.find('.pokemon-detail').text()).toEqual('Pikachu')
+    })
+  })
 })
