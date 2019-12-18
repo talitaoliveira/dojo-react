dfb2294b17b9ad2003dc0c332d02a5db99c16a43
simoneas02 <simoneas02@hotmail.com>
2019-12-18 13:32:00 -0300

    add some style

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 
-const Header = ({title}) => {
-    return (
-        <h1> 
-            {title}
-        </h1>
-    )
-}
-   
+const Header = ({ title }) => {
+  return <h1 className="header">{title}</h1>;
+};
 
-export default Header
+export default Header;
