19ed756b2c5ff10473e2b37911616369f034c558
ShellOliver <sheldon.santosdev@gmail.com>
2019-12-19 09:21:30 -0300

    add example to feth api with route

--- src/List.js
+++ src/List.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './List.css';
+import React from 'react'
+import './List.css'
 
 const List = props => (
-  <ul className="list">
+  <ul className='list'>
     {this.props.pokemon.map(pokemon => {
       return (
         <li
           key={pokemon}
-          className="pokemon-name"
+          className='pokemon-name'
           onClick={() => this.setState({ selectedPokemon: pokemon })}
         >
           {pokemon}
         </li>
-      );
+      )
     })}
   </ul>
-);
+)
 
-export default List;
+export default List
