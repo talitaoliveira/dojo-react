957858e27c8ae5beb023294701e3e596f7f5b9ef
Juliana Cavalcanti <lajuliana.cavalcanti@Julianas-MacBook-Pro.local>
2020-02-06 15:18:31 -0300

    TDD time 06-02

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import './Header.css'
 
-const Header = ({ title }) => (
-  <>
-    <h1 className="header">{title}</h1>
-    <input />
-  </>
-)
+const Header = ({ title }) => {
+
+  const [newTitle, setNewTitle] = useState(title);
+  const titlePrefix = "Pokedex";
+
+  return (
+    <>
+      <h1 className="header">{titlePrefix} {newTitle}</h1>
+      <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+    </>
+  )
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 export default Header
--- src/Header.test.js
+++ src/Header.test.js
@@ -2,16 +2,29 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import Header from './Header'
 
-describe('header', () => {
-  it('should have a title', () => {
-    const wrapper = shallow(<Header title="RAFINHA" />)
-    const title = wrapper.text()
-    expect(title).toEqual('RAFINHA')
-  })
+describe('Header', () => {
 
-  it('should have an input', () => {
-    const wrapper = shallow(<Header />)
+    let wrapper;
 
-    expect(wrapper.find('input')).toHaveLength(1)
-  })
+    beforeEach(() => {
+        wrapper = shallow(<Header title="RAFINHA"/>)
+    });
+
+    it('should have a title', () => {
+        expect(wrapper.text()).toEqual('Pokedex RAFINHA')
+    })
+
+    it('should have an input', () => {
+        expect(wrapper.find('input')).toHaveLength(1)
+    });
+
+    describe('when change input value', () => {
+
+        it('should update the title keeping the prefix', () => {
+            const input = wrapper.find('input');
+            input.simulate('change', { target: { value: 'Juliana' } });
+
+            expect(wrapper.text()).toEqual('Pokedex Juliana');
+        });
+    })
 })
